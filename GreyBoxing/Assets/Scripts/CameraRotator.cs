846c90bdb6483d7cbbe1728e0cd8ae4a454b50cb
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2020-03-04 03:13:36 +0000

    sgdfg

---
+++ GreyBoxing/Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    public float mousesense = 150f;
+    public Transform cameraAxis;
+    public Transform character;
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mousesense * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mousesense * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -50, 10);
+
+        yRotation += mouseX;
+
+        cameraAxis.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        character.Rotate(Vector3.up * mouseX);
+    }
+
+}
---
+++ GreyBoxing/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    public CharacterController character;
+    public float speed = 12f;
+    Vector3 velocity;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float grounddistance = 0.4f;
+    public LayerMask groundmask;
+    public float jumpheight = 3f;
+    public bool isgrounded;
+
+    
+
+    private void Update()
+    {
+        isgrounded = Physics.CheckSphere(groundCheck.position, grounddistance, groundmask);
+        if(isgrounded && velocity.y < 0 )
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 Move = transform.right * x + transform.forward * y;
+        character.Move(Move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isgrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpheight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime * 2;
+        character.Move(velocity * Time.deltaTime);
+    }
+}
