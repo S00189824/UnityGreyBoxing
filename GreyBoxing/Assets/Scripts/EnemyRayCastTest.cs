76377a04ac3f4aac10157147896076b349c06bdf
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2020-03-05 00:38:09 +0000

    NavMesh

---
+++ GreyBoxing/Assets/Scripts/EnemyRayCastTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRayCastTest : MonoBehaviour
+{
+
+    protected RaycastHit raycastHit;
+    protected Ray ray;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ GreyBoxing/Assets/Scripts/NavMeshTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshTest : MonoBehaviour
+{
+
+    public GameObject DeathSpheres;
+    Vector3 position;
+    NavMeshAgent agent;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
